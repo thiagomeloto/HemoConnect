6445af28a4f6a8774c20839ea07bab85a1e87b5d
Thiago Meloto <thiagommeloto@gmail.com>
2025-04-01 00:37:44 -0300

    Implementação do GetAllDonationsQuery e Handler

--- HemoConnect.Application/Queries/GetAllDonations/GetAllDonationsQuery.cs
+++ HemoConnect.Application/Queries/GetAllDonations/GetAllDonationsQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HemoConnect.Core.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace HemoConnect.Application.Queries.GetAllDonations
 {
-    public class GetAllDonationsQuery
+    public class GetAllDonationsQuery : IRequest<List<Donation>>
     {
     }
 }
--- HemoConnect.Application/Queries/GetAllDonations/GetAllDonationsQueryHandler.cs
+++ HemoConnect.Application/Queries/GetAllDonations/GetAllDonationsQueryHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HemoConnect.Core.Entities;
+using HemoConnect.Core.Repositories;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace HemoConnect.Application.Queries.GetAllDonations
 {
-    public class GetAllDonationsQueryHandler
+    public class GetAllDonationsQueryHandler : IRequestHandler<GetAllDonationsQuery, List<Donation>>
     {
+        private readonly IDonationRepository _donationRepository;
+
+        public GetAllDonationsQueryHandler(IDonationRepository donationRepository)
+        {
+            _donationRepository = donationRepository;
+        }
+
+        public async Task<List<Donation>> Handle(GetAllDonationsQuery request, CancellationToken cancellationToken)
+        {
+            return await _donationRepository.GetAllDonationsAsync();
+        }
     }
 }
