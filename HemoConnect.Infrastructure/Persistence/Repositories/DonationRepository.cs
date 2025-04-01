2686849149a8590d21c17b9038d5cde2085991d1
Thiago Meloto <thiagommeloto@gmail.com>
2025-03-31 23:28:03 -0300

    Implementação do DonationRepository

--- HemoConnect.Infrastructure/Persistence/Repositories/DonationRepository.cs
+++ HemoConnect.Infrastructure/Persistence/Repositories/DonationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using HemoConnect.Core.Entities;
 using HemoConnect.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,22 @@ namespace HemoConnect.Infrastructure.Persistence.Repositories
 {
     public class DonationRepository : IDonationRepository
     {
-        public Task<int> AddAsync(Donation donation)
+        private readonly HemoConnectDbContext _dbContext;
+        public DonationRepository(HemoConnectDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+        }
+        public async Task<int> AddAsync(Donation donation)
+        {
+            await _dbContext.Donation.AddAsync(donation);
+            await _dbContext.SaveChangesAsync();
+
+            return donation.Id;
         }
 
-        public Task<List<Donation>> GetAllDonationsAsync()
+        public async Task<List<Donation>> GetAllDonationsAsync()
         {
-            throw new NotImplementedException();
+            return await _dbContext.Donation.ToListAsync();
         }
     }
 }
