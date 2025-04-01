d94967d9135b69f3a3d7f2e86a9a8655bac45623
Thiago Meloto <thiagommeloto@gmail.com>
2025-03-31 22:58:41 -0300

    Criação de todos os commads e queries necessários

---
+++ HemoConnect.Application/Queries/GetAllDonations/GetAllDonationsQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemoConnect.Application.Queries.GetAllDonations
+{
+    public class GetAllDonationsQuery
+    {
+    }
+}
