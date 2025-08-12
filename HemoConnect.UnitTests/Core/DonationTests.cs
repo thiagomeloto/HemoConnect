2b832dfb92c7f91fe9435bc068ba6d630d13f078
Thiago Meloto <thiagommeloto@gmail.com>
2025-08-12 00:07:06 -0300

    Criação dos testes unitários

---
+++ HemoConnect.UnitTests/Core/DonationTests.cs
@@ -0,0 +1,129 @@
+﻿using HemoConnect.Core.Entities;
+using HemoConnect.Infrastructure.Persistence;
+using HemoConnect.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemoConnect.UnitTests.Core
+{
+    public class DonationTests
+    {
+        private HemoConnectDbContext CreateDbContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<HemoConnectDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            return new HemoConnectDbContext(options);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldAddDonation_AndReturnId()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(AddAsync_ShouldAddDonation_AndReturnId));
+            var repository = new DonationRepository(context);
+
+            var donation = new Donation(1, new DateTime(1995, 08, 11), 250);
+
+            //Act
+
+            var result = await repository.AddAsync(donation);
+
+            //Assert
+
+            Assert.NotNull(result);
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public async Task GetAllDonationsAsync_ShouldReturnListOfDonations()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(GetAllDonationsAsync_ShouldReturnListOfDonations));
+            var repository = new DonationRepository(context);
+
+
+            var donation1 = new Donation(1, new DateTime(1995, 08, 11), 250);
+            var donation2 = new Donation(2, new DateTime(1996, 09, 12), 300);
+
+
+            var donation_1 = await repository.AddAsync(donation1);
+            var donation_2 = await repository.AddAsync(donation2);
+
+
+            //Act
+            var result = await repository.GetAllDonationsAsync();
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.True(result.Count > 0);
+        }
+
+        [Fact]
+        public async Task GetDonationByDonorIdAsync_ShouldReturnDonation_WhenExists()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(GetDonationByDonorIdAsync_ShouldReturnDonation_WhenExists));
+            var repository = new DonationRepository(context);
+
+            var donation1 = new Donation(1, new DateTime(1995, 08, 11), 250);
+
+            var donation_1 = await repository.AddAsync(donation1);
+
+            //Act
+
+            var result = await repository.GetDonationByDonorIdAsync(donation1.DonorId);
+
+            //Assert
+
+            Assert.NotNull(result);
+            Assert.True(result[0].AmountML == donation1.AmountML);
+        }
+
+        [Fact]
+        public async Task GetDonationByIdAsync_ShouldReturnDonation_WhenExists()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(GetDonationByIdAsync_ShouldReturnDonation_WhenExists));
+            var repository = new DonationRepository(context);
+
+            var donation1 = new Donation(1, new DateTime(1995, 08, 11), 250);
+            var donation_1 = await repository.AddAsync(donation1);
+
+            //Act
+            var result = await repository.GetDonationByIdAsync(donation_1);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Id == donation1.Id);
+        }
+
+        [Fact]
+        public async Task GetLastDonationByDonorIdAsync_ShouldReturnLastDonation_WhenExists()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(GetLastDonationByDonorIdAsync_ShouldReturnLastDonation_WhenExists));
+            var repository = new DonationRepository(context);
+
+            var donation1 = new Donation(1, new DateTime(1995, 08, 11), 250);
+            var donation2 = new Donation(1, new DateTime(1996, 09, 12), 300);
+
+            await repository.AddAsync(donation1);
+            await repository.AddAsync(donation2);
+
+            //Act
+            var result = await repository.GetLastDonationByDonorIdAsync(donation1.DonorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.DonationDate == donation2.DonationDate);
+        }
+    }
+}
---
+++ HemoConnect.UnitTests/Core/DonorTests.cs
@@ -0,0 +1,111 @@
+﻿using HemoConnect.Core.Entities;
+using HemoConnect.Infrastructure.Persistence;
+using HemoConnect.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemoConnect.UnitTests.Core
+{
+    public class DonorTests
+    {
+
+        private HemoConnectDbContext CreateDbContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<HemoConnectDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            return new HemoConnectDbContext(options);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldAddDonor_AndReturnId()
+        {
+            //Arrange: Preparar: Criar objetos que serão testados/Criar dados de entrada/Configurar mocks/stubs/fakes
+
+            var context = CreateDbContext(nameof(AddAsync_ShouldAddDonor_AndReturnId));
+
+            var repository = new DonorRepository(context);
+
+            var donor = new Donor("Thiago Mendes Meloto", "thiagomeloto@gmail.com", new DateTime(1995, 08, 11), "M", 72.0, "0", "Negativo");
+
+            //Act: Chamar o método ou função que está sendo testado/Passar os parâmetros preparados na etapa Arrange
+
+            var id = await repository.AddAsync(donor);
+
+            //Assert: Verificar: confirmar se o resultado obtido é o esperado./Usar asserts (por exemplo, Assert.Equal, result.Should().Be(...)) para verificar saídas
+
+            Assert.NotNull(id);
+            Assert.True(id > 0);
+
+            var result = await context.Donors.FindAsync(id);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnDonor_WhenExists()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(GetByIdAsync_ShouldReturnDonor_WhenExists));
+            var repository = new DonorRepository(context);
+
+            var donor = new Donor("Thiago Mendes Meloto", "thiagomeloto@gmail.com", new DateTime(1995, 08, 11), "M", 72.0, "0", "Negativo");
+
+            var id = await repository.AddAsync(donor);
+
+            //Act
+
+            var result = await repository.GetByIdAsync(id);
+
+            //Assert
+
+            Assert.NotNull(result);
+            Assert.Equal(id, result.Id);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnTrue_WhenExists()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(GetByIdAsync_ShouldReturnTrue_WhenExists));
+            var repository = new DonorRepository(context);
+
+            var donor = new Donor("Thiago Mendes Meloto", "thiagomeloto@gmail.com", new DateTime(1995, 08, 11), "M", 72.0, "0", "Negativo");
+
+            var id = await repository.AddAsync(donor);
+
+            //Act
+
+            var result = await repository.ExistsAsync(id);
+
+            //Assert
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task EmailExistsAsync_ShouldReturnTrue_WhenEmailExists()
+        {
+            //Arrange
+            var context = CreateDbContext(nameof(EmailExistsAsync_ShouldReturnTrue_WhenEmailExists));
+            var repository = new DonorRepository(context);
+            var donor = new Donor("Thiago Mendes Meloto", "thiagomeloto@gmail.com", new DateTime(1995, 08, 11), "M", 72.0, "0", "Negativo");
+
+            var id = await repository.AddAsync(donor);
+
+            //Act
+
+            var result = await repository.EmailExistsAsync(donor.Email);
+
+            //Assert
+
+            Assert.True(result);
+        }
+
+    }
+}
